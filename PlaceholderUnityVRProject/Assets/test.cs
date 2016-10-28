5fd550b013111bdbd0e8c1bea8f37406b6c97106
atulmorey <atul.s.morey@gmail.com>
2016-10-28 09:59:06 -0700

    VRSamplesProject
    Downloaded VRSamples project from Unity website and modified it

--- PlaceholderUnityVRProject/Assets/test.cs
+++ PlaceholderUnityVRProject/Assets/test.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.VR;
 
-public class test : MonoBehaviour {
+public class test : MonoBehaviour
+{
+   
+
 
 	// Use this for initialization
 	void Start () {
 	
+       
 	}
 	
 	// Update is called once per frame
---
+++ VRSamples/Assets/Teleport.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+using VRStandardAssets.Utils;
+
+public class Teleport : MonoBehaviour
+{
+    public GameObject MainCameraGameObject;
+
+    private VRInteractiveItem _vrInteractiveItem;
+
+    void OnEnable()
+    {
+        _vrInteractiveItem.OnDoubleClick += DoTeleport;
+    }
+
+    void OnDisable()
+    {
+        _vrInteractiveItem.OnDoubleClick -= DoTeleport;
+    }
+
+    void DoTeleport()
+    {
+        Vector3 teleTarget = new Vector3(this.transform.position.x, MainCameraGameObject.transform.localPosition.y, this.transform.position.z);
+
+        MainCameraGameObject.transform.localPosition = teleTarget;
+
+
+    }
+}
