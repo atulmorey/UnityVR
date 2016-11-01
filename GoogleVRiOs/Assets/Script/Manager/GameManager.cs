03267016f4b20da206b7338b57d4b370e68d4b33
Atul More <atul.s.morey@gmail.com>
2016-11-01 16:58:12 -0700

    SceneTransitionWorking

---
+++ GoogleVRiOs/Assets/Script/Manager/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour 
+{
+    public static GameManager Instance = null;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != null)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+    //Load the new scene
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+
+        //Load
+        GameObject.Find("Loader").GetComponent<Loader>().Load();
+    }
+}
---
+++ GoogleVRiOs/Assets/Script/Manager/Loader.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loader : MonoBehaviour 
+{
+    public GameObject GameManagerObj;
+
+    void Awake()
+    {
+        if (GameManager.Instance == null)
+        {
+            Load();
+        }
+    }
+
+    public void Load()
+    {
+        Instantiate(GameManagerObj);
+    }
+}
